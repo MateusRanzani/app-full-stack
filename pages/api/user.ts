a2136174b2249e6c024cb38861f2d48d32660d5f
MateusRanzani <mateusranzani@hotmail.com>
2022-08-25 09:50:47 -0300

    Feature: Create user api route

---
+++ pages/api/user.ts
@@ -0,0 +1,42 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from "next";
+import connect from "../../utils/database";
+
+type ErrorResponseType = {
+  error: string;
+};
+
+type SuccessResponseType = {
+  _id: string;
+  name: string;
+  email: string;
+  cellphone: string;
+  teacher: string;
+};
+
+export default async (
+  req: NextApiRequest,
+  res: NextApiResponse<ErrorResponseType | SuccessResponseType>
+): Promise<void> => {
+  if (req.method === "POST") {
+    const { name, email, cellphone, teacher } = req.body;
+
+    if (!name || !email || !cellphone || !teacher) {
+      res.status(400).json({ error: "Missing body parameter" });
+      return;
+    }
+
+    const { db } = await connect();
+
+    const response = await db.collection("users").insertOne({
+      name,
+      email,
+      cellphone,
+      teacher,
+    });
+
+    res.status(200).json(response.ops[0]);
+  } else {
+    res.status(400).json({ error: "Wrong request method" });
+  }
+};
